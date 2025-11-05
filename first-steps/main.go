e13a1e465499ca111de200f2f068a2ac8c7bbadc
RogerJackD <rogerzapata114@gmail.com>
2025-11-04 23:38:55 -0500

    first commit

---
+++ first-steps/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello word! Golang")
+
+	const miNumero int = 123
+
+	fmt.Println("mi numero es:", miNumero)
+
+}
+
+// comando para ejecutar archivo go: go run main.go
