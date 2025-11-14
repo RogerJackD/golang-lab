e930a72877796ff37fd10f52231fa45622f95aa6
RogerJackD <rogerzapata114@gmail.com>
2025-11-13 23:23:58 -0500

    practice: add new data-structure map

---
+++ first-steps/data-structure/map.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func contadorPalabras(palabras []string) map[string]int {
+	contador := make(map[string]int)
+
+	for _, palabra := range palabras {
+		contador[palabra]++
+	}
+
+	return contador
+}
+
+func main() {
+	lista := []string{"go", "java", "go", "python", "go", "java"}
+	fmt.Println(contadorPalabras(lista))
+}
