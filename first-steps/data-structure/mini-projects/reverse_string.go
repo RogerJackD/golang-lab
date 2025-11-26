f1690de4bd93e8e0aef50f7b711e6083ad141efe
RogerJackD <rogerzapata114@gmail.com>
2025-11-25 23:33:17 -0500

    feat: add function to reverse a string

---
+++ first-steps/data-structure/mini-projects/reverse_string.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func reverse(text string) string {
+	runes := []rune(text)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	input := "golang"
+	fmt.Println("Original:", input)
+	fmt.Println("Reversed:", reverse(input))
+}
